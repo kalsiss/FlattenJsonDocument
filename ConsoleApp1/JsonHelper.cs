69009fbf4cbe53fd8bc31e189a90396d7b8be80e
Satty S <kalsiss@gmail.com>
2024-10-16 10:44:06 +1100

    initial commit

---
+++ ConsoleApp1/JsonHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class JsonHelper
+    {
+        public static Dictionary<string, JsonElement> Flatten(this JsonElement element, string previousPath = null)
+        {
+            var result = new Dictionary<string, JsonElement>();
+
+            foreach (var prop in element.EnumerateObject())
+            {
+                var currentPropName = previousPath != null ? previousPath + "." + prop.Name : prop.Name;
+                result.Add(currentPropName, prop.Value);
+
+                if (prop.Value.ValueKind == JsonValueKind.Object)
+                {
+                    var innerDictionary = prop.Value.Flatten(currentPropName);
+                    foreach(var inner in innerDictionary)
+                    {
+                        result.Add(inner.Key, inner.Value);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static JsonElement GetFileAsJsonDocument(this string filePath)
+        {
+            var fileContent = File.ReadAllText(filePath);
+            return JsonDocument.Parse(fileContent).RootElement;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Read a Json Document file and flatten it into a Dictionary with Keys and Values");
+var filePath = "JsonDocExample1.json";
+var jsonDocConent = filePath.GetFileAsJsonDocument();
+var flattenJsonContent = jsonDocConent.Flatten();
+Console.WriteLine("Dispaly flattened Json");
+foreach (var item in flattenJsonContent)
+{
+    Console.Write("key:" + item.Key);
+    Console.WriteLine(", value:" + item.Value);
+
+}
+Console.ReadLine();
